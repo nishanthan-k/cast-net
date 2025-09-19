ea09286d14e9ab71ee4790573850182687b53147
nishanthan-k <krn.nishanthan@gmail.com>
2025-09-19 06:56:16 +0530

    feat: spring setup

---
+++ src/main/java/com/nishanthan/castnet/CastNetApplication.java
@@ -0,0 +1,13 @@
+package com.nishanthan.castnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastNetApplication.class, args);
+	}
+
+}
